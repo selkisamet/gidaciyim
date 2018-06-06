02639930e6cf3efc4b3e5d43fbee1e7df0518f5b
Samet <sametselki@outlook.com>
2018-06-06 20:30:58 +0300

    Blog Tamamlandi

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -12,7 +12,7 @@ class KategoriController extends Controller
     public function index($slug)
     {
     	$kategori = Kategori::where('slug',$slug)->first();
-    	$makaleler = Makale::where('kategori_id',$kategori->id)->where('durum',1)->paginate(10);
+    	$makaleler = Makale::where('kategori_id',$kategori->id)->where('durum',1)->orderBy('created_at','desc')->paginate(10);
     	return view('kategori',compact('kategori','makaleler'));
     }
 }
